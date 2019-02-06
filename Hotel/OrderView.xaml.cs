f6e192ef05cd4b8e45061e2d2d8d1cb1f242e9f7
Kasper Kraack <MrKraack@gmail.com>
2019-02-06 11:38:19 +0100

    yoyoyo

--- Hotel/OrderView.xaml.cs
+++ Hotel/OrderView.xaml.cs
@@ -22,23 +22,8 @@ namespace Hotel
         public OrderView()
         {
             InitializeComponent();
-            List<Room> rooms = Order.RoomList();
-            Room low = new Low("Low budget room");
-            low.Pris = 100;
-            low.AntalSovePladser = 10;
-            rooms.Add(low);
 
-            Room med = new Med("Medium budget room");
-            med.Pris = 10000;
-            med.AntalSovePladser = 4;
-            rooms.Add(med);
-
-            Room high = new High("High budget room");
-            high.Pris = 100000;
-            high.AntalSovePladser = 2;
-            rooms.Add(high);
-
-            HotelList.ItemsSource = rooms;
+            HotelList.ItemsSource = Order.RoomList();
 
         }
     }
